37ee4d335152a3a2822ddd471c796be96e4d4706
LongAn <longan18.dev@gmail.com>
2025-09-11 09:05:43 +0700

    first commit

--- backend/services/store.go
+++ backend/services/store.go
@@ -35,6 +35,8 @@ func StoreNote() gin.HandlerFunc {
 		note.SetID()
 		models.Add(note)
 
+		listFile = []models.File{}
+
 		ctx.JSON(constants.StatusOK, gin.H{
 			"message": "Add note success",
 			"data": note,
@@ -77,3 +79,32 @@ func uploadFile(ctx *gin.Context) error {
 
 	return nil
 }
+
+// func uploadMultipleFile(ctx *gin.Context) {
+// 	form, _ := ctx.MultipartForm()
+// 	files := form.File["images"]
+// 	filePaths := []string{}
+// 	for _, file := range files {
+// 		fileExt := filepath.Ext(file.Filename)
+// 		originalFileName := strings.TrimSuffix(filepath.Base(file.Filename), filepath.Ext(file.Filename))
+// 		now := time.Now()
+// 		filename := strings.ReplaceAll(strings.ToLower(originalFileName), " ", "-") + "-" + fmt.Sprintf("%v", now.Unix()) + fileExt
+// 		filePath := "http://localhost:8080/backend/file/" + filename
+
+// 		filePaths = append(filePaths, filePath)
+// 		out, err := os.Create("./public/multiple/" + filename)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		defer out.Close()
+
+// 		readerFile, _ := file.Open()
+// 		_, err = io.Copy(out, readerFile)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+
+// 	ctx.JSON(http.StatusOK, gin.H{"filepath": filePaths})
+// }
+
